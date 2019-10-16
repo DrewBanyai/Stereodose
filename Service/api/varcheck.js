3c3c955fa9b6726d5d863c4a0875a26c303f8341
Drew Banyai <DrewBanyai@gmail.com>
2019-10-15 18:56:47 -0700

    Added console output debugging for VarCheck

--- Service/api/varcheck.js
+++ Service/api/varcheck.js
@@ -1,1 +1,6 @@
-exports.check = (varName, type, body) => { return (body.hasOwnProperty(varName) && (Object.prototype.toString.call(body[varName]) === `[object ${type}]`)); }
+exports.check = (varName, type, body) => {
+    if (!body.hasOwnProperty(varName)) { console.log("Body has no variable named " + varName); return false; }
+    let varType = Object.prototype.toString.call(body[varName]);
+    if (varType != `[object ${type}]`) { console.log("VarCheck: Expected " + `[object ${type}]` + ", got " + varType); return false;}
+    return true;
+}
