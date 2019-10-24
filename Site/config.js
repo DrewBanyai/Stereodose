15c42dca334a0d7a434f03faf1d5795775b5ffe8
Drew Banyai <DrewBanyai@gmail.com>
2019-10-23 21:29:25 -0700

    - Updated the site config to connect to the microservice server

--- Site/config.js
+++ Site/config.js
@@ -4,7 +4,7 @@ let config = {
     MaximumSongsPerPlaylist: 12,
     
     //  MICROSERVICE
-    MicroserviceURL: "http://127.0.0.1:3000/",
+    MicroserviceURL: "http://98.181.198.246:3000/",
     LoginEndpoint: "players/login",
     MicroserviceRequiresLogin: false,
     LogoutTimerMilliseconds: 86400000,      //  24 hours
