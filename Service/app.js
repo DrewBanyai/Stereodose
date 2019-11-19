0a2ab359fe85f56b00714372c488214f96826131
Drew Banyai <DrewBanyai@gmail.com>
2019-11-18 19:21:47 -0800

    Returned to the old method of handling CORS in-code

--- Service/app.js
+++ Service/app.js
@@ -14,6 +14,17 @@ app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin, Cache-Control, Access-Control-Allow-Headers, Access-Control-Request-Method, Access-Control-Request-Headers, Origin, X-Requested-With, X-Powered-By, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Credentials', 'true');
+        res.header('Access-Control-Allow-Methods', 'POST');
+        return res.status(200).json({});
+    }
+    next();
+});
+
 //  Routes which should handle requests
 app.use("/user", userRoutes);
 app.use("/playlist", playlistRoutes);
