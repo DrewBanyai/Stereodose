b1c1b34a0c51512e684e68e76283371ba8af13c7
Drew Banyai <DrewBanyai@gmail.com>
2019-11-19 13:24:41 -0800

    Updating CheckAuth to user a non jwt authorization method (to avoid CORS issues?)

--- Service/api/middleware/checkAuth.js
+++ Service/api/middleware/checkAuth.js
@@ -1,6 +1,13 @@
 const jwt = require("jsonwebtoken");
 
+let tokenMap = {};
+
+exports.saveOffToken = async (username, token) => { tokenMap[token] = username; }
+
 module.exports = (req, res, next) => {
-    try { req.userAuthorization = jwt.verify(req.body.token, process.env.JWT_KEY); next(); }
-    catch (error) {  return res.status(200).json({ success: false, message: "Authorization failed", }); }
+    try { req.userAuthorization = (tokenMap.hasOwnProperty(req.body.token) ? true : false); next(); }
+    catch (error) { return res.status(200).json({ success: false, message: "Authorization failed", }); }
+    
+    //try { req.userAuthorization = jwt.verify(req.body.token, process.env.JWT_KEY); next(); }
+    //catch (error) {  return res.status(200).json({ success: false, message: "Authorization failed", }); }
 }
